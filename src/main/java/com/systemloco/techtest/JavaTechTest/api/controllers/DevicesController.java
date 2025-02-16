fdc1bd35db3fefe0fa070f7b46dcdc30c6765864
Tom Mooney <mrtm84@protonmail.com>
2025-02-16 23:32:31 +0000

    Continuing to clean the code up

--- src/main/java/com/systemloco/techtest/JavaTechTest/api/controllers/DevicesController.java
+++ src/main/java/com/systemloco/techtest/JavaTechTest/api/controllers/DevicesController.java
@@ -1,40 +1,38 @@
 package com.systemloco.techtest.JavaTechTest.api.controllers;
 
-import java.util.Collection;
-import java.util.Optional;
-
-import org.jetbrains.annotations.NotNull;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
 import com.systemloco.techtest.JavaTechTest.api.services.DevicesService;
-
-import lombok.RequiredArgsConstructor;
 
 import static com.systemloco.techtest.JavaTechTest.api.services.DevicesService.Response;
 
+import java.util.*;
+
+import lombok.RequiredArgsConstructor;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
 @RestController
+@RequestMapping("/api/devices")
 @RequiredArgsConstructor
 public class DevicesController {
     @NotNull
     private final DevicesService service;
 
-    @GetMapping("/api/devices")
+    @GetMapping
     @ResponseBody
     Collection<Response> findAllActiveDevices() {
         return service.findAllActiveDevices();
     }
 
-    @GetMapping("/api/devices/{id}")
+    @GetMapping("/{id}")
     @ResponseBody
     Response findActiveDeviceById(@PathVariable("id") @NotNull final String deviceId) {
-        var device = service.findActiveDeviceById(deviceId);
+        var activeDevice = service.findActiveDeviceById(deviceId);
+
         return Optional
-                .ofNullable(device)
+                .ofNullable(activeDevice)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
