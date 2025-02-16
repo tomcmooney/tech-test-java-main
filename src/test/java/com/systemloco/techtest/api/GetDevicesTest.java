217d73b676cd08755c789993f443c4d999f45a76
Tom Mooney <mrtm84@protonmail.com>
2025-02-16 15:56:29 +0000

    Added a failing GetDevicesTest due to a 404 (Not Found)

---
+++ src/test/java/com/systemloco/techtest/api/GetDevicesTest.java
@@ -0,0 +1,34 @@
+package com.systemloco.techtest.api;
+
+import com.systemloco.techtest.JavaTechTest.Application;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.hamcrest.Matchers.empty;
+
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureDataMongo
+@ActiveProfiles("test")
+@DisplayName("GET /api/devices")
+public class GetDevicesTest {
+        @LocalServerPort
+        int port;
+
+        @BeforeEach
+        public void beforeEach() {
+                RestAssured.port = port;
+        }
+
+        @Test
+        public void whenGetDevices_returnsOk_withEmptyDevicesList() {
+                RestAssured.given()
+                                .get("/api/devices")
+                                .then()
+                                .statusCode(200)
+                                .body(empty());
+        }
+}
